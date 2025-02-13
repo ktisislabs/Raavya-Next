648ef17325f4c943b15caba9bd31274195fcea3c
adixb <aditya.bhattacharjee706@gmail.com>
2025-02-13 13:59:55 +0530

    updated code

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import Loader from '../app/Loader/page'
+import Head from 'next/head'
 
 function page() {
   return (
     <>
+    <Head>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
 <Loader/>
 
